4e69201bd34788e08cd9e2a41c27324d4371d057
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2023-09-11 06:40:12 +0200

    regractoring extracted get and set methods

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -1,154 +1,23 @@
-import axios, { AxiosResponse} from "axios";
-const url = 'http://localhost:3000/users/'
- 
+import axios, { AxiosPromise } from "axios";
 
-export class Sync {
-  
-  fetch(): void {
-    axios.get(url + this.get('id'))
-    .then((response: AxiosResponse): void => {
-      this.set(response.data);
-    });
+interface HasId {
+  id?: number;
+}
+
+export class Sync<T extends HasId> {
+
+  constructor (public rootUrl: string) {}
+   
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.rootUrl}/${id}`);
   }
 
-  save(): void {
-    let id = this.get('id');
+  save(data: T): AxiosPromise {
+    let { id } = data;  
     if (id) {
-      axios.put(url + id, this.data);
+      return axios.put(`${this.rootUrl}/${id}`, data);
     } else {
-      axios.post(url, this.data);
+      return axios.post(`${this.rootUrl}/${id}`, data);
     }
   } 
-
-
-} 
-
-
-/* fetch(): void {
-    const id = this.get('id');
-    if (id) {
-      const requestURL = url + id;
-      axios.get(requestURL)
-        .then((response: AxiosResponse): void => {
-          this.set(response.data);
-          console.log('Fetch successful:', requestURL);
-        })
-        .catch((error) => {
-          console.error('Fetch Error:', requestURL, error);
-        });
-      }
-    }
-  
-  save(): void {
-    const id = this.get('id');
-    if (id) {
-      axios.put(url + id, this.data)
-        .catch((error) => {
-          console.error('Axios Error:', error);
-        });
-    } else {
-      axios.post(url, this.data)
-        .catch((error) => {
-          console.error('Axios Error:', error);
-        });
-      }
-    }
-  }
-*/
-
-/* 
-  fetch(): void {
-    const id = this.get('id');
-    if (id) {
-      axios.get(url+id)
-        .then((response: AxiosResponse): void => {
-          this.set(response.data);
-        })
-        .catch((error) => {
-          console.error('Axios Error:', error);
-        });
-    }
-  }
-  
-  save(): void {
-    const id = this.get('id');
-    if (id) {
-      axios.put(url+id, this.data)
-        .catch((error) => {
-          console.error('Axios Error:', error);
-        });
-    } else {
-      axios.post(url, this.data)
-        .catch((error) => {
-          console.error('Axios Error:', error);
-        });
-    }
-  } 
-  */
- 
-/* fetch(): void {
-    const id = this.get('id');
-    if (id) {
-      const requestURL = url + id;
-      axios.get(requestURL)
-        .then((response: AxiosResponse): void => {
-          this.set(response.data);
-          console.log('Fetch successful:', requestURL);
-        })
-        .catch((error) => {
-          console.error('Fetch Error:', requestURL, error);
-        });
-      }
-    }
-  
-  save(): void {
-    const id = this.get('id');
-    if (id) {
-      axios.put(url + id, this.data)
-        .catch((error) => {
-          console.error('Axios Error:', error);
-        });
-    } else {
-      axios.post(url, this.data)
-        .catch((error) => {
-          console.error('Axios Error:', error);
-        });
-      }
-    }
-  }
-*/
-
-/* 
-  fetch(): void {
-    const id = this.get('id');
-    if (id) {
-      axios.get(url+id)
-        .then((response: AxiosResponse): void => {
-          this.set(response.data);
-        })
-        .catch((error) => {
-          console.error('Axios Error:', error);
-        });
-    }
-  }
-  
-  save(): void {
-    const id = this.get('id');
-    if (id) {
-      axios.put(url+id, this.data)
-        .catch((error) => {
-          console.error('Axios Error:', error);
-        });
-    } else {
-      axios.post(url, this.data)
-        .catch((error) => {
-          console.error('Axios Error:', error);
-        });
-    }
-  } 
-  */
-  
-/* 
-  public async fetch(): Promise<void> {
-  const response = await axios.get(`http://localhost:3000/users/${this.get('id')}`)
-  this.set(response.data) */
+}
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,28 +1,18 @@
 import { Eventing } from "./Eventing";
+import { Sync } from "./Sync";
 
-interface UserProps {
+export interface UserProps {
   id? : number;
   name? : string;
   age? : number;
 }
 
+const rootUrl = 'http://localhost:3000/users';
+
 export class User {
 
   public events: Eventing = new Eventing();
-
-  constructor(private data: UserProps) {};
-
-  get<K extends keyof UserProps>(propName: K): number | string | void {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl)
 }
 
-/*
-{
-  "users" : []
-}
-*/
+// { "users" : [] }
